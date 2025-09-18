7ead26f72ce0a414d2c0438cff045bfd409c36e3
chinmay116 <chinmaydesle03@gmail.com>
2025-09-18 19:50:16 +0530

    Vercel

--- index.py
+++ index.py
@@ -34,6 +34,3 @@ def index():
         return render_template('index.html', prediction=prediction[0])
     
     return render_template("index.html",prediction=None)
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
